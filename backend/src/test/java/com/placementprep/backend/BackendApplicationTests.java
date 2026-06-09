937627c6a4bbc8501b3df453abfa006f40a10fba
gajeshM-27 <rajagajesh@gmail.com>
2026-06-09 22:45:14 +0530

    Initial project setup

---
+++ backend/src/test/java/com/placementprep/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.placementprep.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
